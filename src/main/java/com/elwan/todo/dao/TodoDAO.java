f0a183374f363304c891b4900769865a98cc2a8d
EG01\Mostafa.Elwan <mustafa.m.abdelsattar@gmail.com>
2017-09-13 04:27:49 +0200

    Throws API Exception

--- src/main/java/com/elwan/todo/dao/TodoDAO.java
+++ src/main/java/com/elwan/todo/dao/TodoDAO.java
@@ -2,11 +2,12 @@ package com.elwan.todo.dao;
 
 import java.util.List;
 
+import com.elwan.todo.exception.APIException;
 import com.elwan.todo.model.Todo;
 import com.elwan.todo.model.User;
 
 public interface TodoDAO extends DAO<Todo> {
 	
-	List<Todo> all(User user);
+	List<Todo> all(User user) throws APIException;
 
 }
