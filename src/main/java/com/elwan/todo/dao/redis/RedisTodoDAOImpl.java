ece49070c7bc5808f565fd7f185d48a5cba23b63
EG01\Mostafa.Elwan <mustafa.m.abdelsattar@gmail.com>
2017-09-13 04:30:37 +0200

    Use INCR method in addition to  ...
    1- adding the Id of the created todo into  a Set for the user
    2- Remove the todo from user Set while deleting todo item

--- src/main/java/com/elwan/todo/dao/redis/RedisTodoDAOImpl.java
+++ src/main/java/com/elwan/todo/dao/redis/RedisTodoDAOImpl.java
@@ -1,20 +1,24 @@
 package com.elwan.todo.dao.redis;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 import org.apache.commons.beanutils.BeanUtilsBean;
 import org.springframework.stereotype.Component;
 
 import redis.clients.jedis.Jedis;
+import redis.clients.jedis.Transaction;
 
+import com.elwan.todo.common.AppConstant;
 import com.elwan.todo.dao.TodoDAO;
 import com.elwan.todo.exception.APIException;
 import com.elwan.todo.model.Todo;
 import com.elwan.todo.model.User;
 
 @Component
-public class RedisTodoDAOImpl extends AbstractRedisDAO implements TodoDAO {
+public class RedisTodoDAOImpl extends AbstractRedisDAO<Todo> implements TodoDAO {
 
 	@Override
 	public List<Todo> all() {
@@ -31,21 +35,25 @@ public class RedisTodoDAOImpl extends AbstractRedisDAO implements TodoDAO {
 			BeanUtilsBean.getInstance().populate(todo, props);
 			return todo;
 		} catch(Exception e) {
-			throw new APIException("Error while creating TODO", e);
+			throw new APIException(String.format("Error while fetching TODO [%s]", id), e);
 		}
 	}
 
 	@Override
-	public long create(Todo t) throws APIException {
+	public long create(Todo todo) throws APIException {
 		try {
-			long todoId = getTodoId();
-			t.setId(todoId);
 			Jedis j = getJedis();
-			Map<String, String> props = BeanUtilsBean.getInstance().describe(t);
-			j.hmset(String.format("TODO:%s", todoId), props);
+			long todoId = j.incr(AppConstant.Redis.Keys.TODO_ID);
+			todo.setId(todoId);
+			
+			Transaction t = j.multi();
+			Map<String, String> props = BeanUtilsBean.getInstance().describe(todo);
+			t.hmset(String.format("TODO:%s", todoId), props);
+			t.sadd(String.format("USER-TODO-LIST:%s", todo.getUser().getId()), String.valueOf(todo.getId()));
+			t.exec();
 			return todoId;
 		} catch(Exception e) {
-			throw new APIException("Error while creating TODO", e);
+			throw new APIException(String.format("Error while creating TODO [%s]", todo.getTitle()), e);
 		}
 	}
 
@@ -56,15 +64,37 @@ public class RedisTodoDAOImpl extends AbstractRedisDAO implements TodoDAO {
 	}
 
 	@Override
-	public void delete(long id) {
-		// TODO Auto-generated method stub
-		
+	public void delete(Todo todo) throws APIException {
+		try {
+			Jedis j = getJedis();
+			
+			Transaction t = j.multi();
+			t.srem(String.format("USER-TODO-LIST:%s", todo.getUser().getId()), String.valueOf(todo.getId()));
+			t.del(String.format("TODO:%s", todo.getId()));
+			t.exec();
+		} catch(Exception e) {
+			throw new APIException(String.format("Error while creating TODO [%s]", todo.getTitle()), e);
+		}
 	}
 
 	@Override
-	public List<Todo> all(User user) {
-		// TODO Auto-generated method stub
-		return null;
+	public List<Todo> all(User user) throws APIException {
+		List<Todo> todoList = new ArrayList<Todo>();
+		try {
+			Todo todo = null;
+			Jedis j = getJedis();
+			Set<String> todoIds = j.smembers(String.format("USER-TODO-LIST:%s", user.getId()));
+			for(String todoId : todoIds) {
+				todo = new Todo();
+				Map<String, String> props = j.hgetAll(String.format("TODO:%s", todoId));
+				props.remove("user");
+				BeanUtilsBean.getInstance().populate(todo, props);
+				todoList.add(todo);
+			}
+			return todoList;
+		} catch(Exception e) {
+			throw new APIException(String.format("Error while fetching todo list of user [%s]", user.getId()), e);
+		}
 	}
 
 }
